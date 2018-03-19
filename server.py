ae28fc9ffc0bb13468e7b27cd22dc1d196dc368c
Matt Werenski <MattWerenski@github.com>
2018-03-19 15:43:06 +0000

    Cleaned out images and old files leaving only essential scripts in the repo

--- server.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = "http://ec2-18-219-63-23.us-east-2.compute.amazonaws.com/development/instructionsPost"
-data = {'instruction': 'MOVE', 'value': '500'}
-r = requests.post(url, data = data)
-print (r.text)
--- settings.py
+++
@@ -1,4 +0,0 @@
-import cv2
-vid = cv2.VideoCapture(0)
-for i in range(16):
-    print('\t\t('+str(i)+','+str(vid.get(i))+')')
